a1aa76c60a85375ebba72488e421dbbdfd0c1d96
ameliawaruwu <ameliawaruwu50@gmail.com>
2025-10-17 12:32:32 +0700

    Merge conflicts resolved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,32 +30,3 @@
     @yield('scripts')
 </body>
 </html>
-<!doctype html>
-<html lang="id">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GoMaggot - @yield('title')</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css" rel="stylesheet">
-</head>
-<body>
-
-    {{-- Navbar --}}
-    <x-navbar />
-
-    {{-- Main Content --}}
-    <main>
-        @yield('content')
-    </main>
-
-    {{-- Footer --}}
-    @include('layouts.footer')
-
-    <script src="{{ asset('js/script.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
