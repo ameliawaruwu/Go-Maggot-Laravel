e0a3f851b28c91637c012d473dd1426a93334794
ameliawaruwu <ameliawaruwu50@gmail.com>
2025-11-22 13:59:25 +0700

    membuat folder photo dan berkas untuk upload foto

--- routes/web.php
+++ routes/web.php
@@ -46,60 +46,23 @@ Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashb
 // Analytics
 Route::get('/analytics', [AnalyticsController::class, 'index'])->name('analytics.index');
 
-// Manage Publication
-Route::prefix('manageprod')->group(function () {
-    Route::get('/', [ManageProductsController::class, 'index'])->name('products.index'); 
-    Route::get('/tambah', [ManageProductsController::class, 'create'])->name('products.create');
-    Route::post('/', [ManageProductsController::class, 'store'])->name('products.store'); 
-    Route::get('/{id}/edit', [ManageProductsController::class, 'edit'])->name('products.edit');
-    Route::put('/{id}', [ManageProductsController::class, 'update'])->name('products.update');
-    Route::delete('/{id}', [ManageProductsController::class, 'destroy'])->name('products.destroy');
-});
+// Manage Produk Admin
+Route::get('/manageProduk', [ManageProductsController::class, 'index']);
+
+
 
 // Manage Gallery
-Route::prefix('managegaleri')->group(function () {
-    Route::get('/', [ManageGalleryController::class, 'index'])->name('gallery.index');
-    Route::get('/tambah2', [ManageGalleryController::class, 'create'])->name('gallery.create');
-    Route::post('/', [ManageGalleryController::class, 'store'])->name('gallery.store');
-    Route::get('/{id}/edit', [ManageGalleryController::class, 'edit'])->name('gallery.edit');
-    Route::put('/{id}', [ManageGalleryController::class, 'update'])->name('gallery.update');
-    Route::delete('/{id}', [ManageGalleryController::class, 'destroy'])->name('gallery.destroy');
-});
+
 
 // Manage Publication
-Route::prefix('managepublication')->name('publication.')->group(function () {
-    Route::get('/', [ManagePublicationController::class, 'index'])->name('index');
-    Route::get('/create', [ManagePublicationController::class, 'create'])->name('create');
-    Route::post('/', [ManagePublicationController::class, 'store'])->name('store');
-    Route::get('/{id}/edit', [ManagePublicationController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [ManagePublicationController::class, 'update'])->name('update');
-    Route::delete('/{id}', [ManagePublicationController::class, 'destroy'])->name('destroy');
-});
+
 
 // Manage User
-Route::prefix('manageuser')->name('user.')->group(function () {
-    Route::get('/', [ManageUserController::class, 'index'])->name('index');
-    Route::get('/create', [ManageUserController::class, 'create'])->name('create');
-    Route::post('/', [ManageUserController::class, 'store'])->name('store');
-    Route::get('/{id}/edit', [ManageUserController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [ManageUserController::class, 'update'])->name('update');
-    Route::delete('/{id}', [ManageUserController::class, 'destroy'])->name('destroy');
-});
+
 
 // Manage Review
-Route::prefix('managereview')->name('review.')->group(function () {
-    Route::get('/', [ManageReviewController::class, 'index'])->name('index');
-    Route::put('/{id}/status', [ManageReviewController::class, 'updateStatus'])->name('updateStatus');
-    Route::delete('/{id}', [ManageReviewController::class, 'destroy'])->name('destroy');
-});
 
 // Manage Faq
-Route::resource('managefaq', ManageFaqController::class)->names([
-    'index' => 'managefaq.index',    
-    'store' => 'managefaq.store',    
-    'update' => 'managefaq.update', 
-    'destroy' => 'managefaq.destroy',
-]);
 
 // Manage Setting
 Route::get('/managesetting', [ManageSettingController::class, 'index'])->name('settings.index');
