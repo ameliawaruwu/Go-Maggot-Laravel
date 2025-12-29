974b44e6ab023b8b06565fcd2b2d044977cbcf03
ameliawaruwu <ameliawaruwu50@gmail.com>
2025-12-29 22:15:31 +0700

    tambahan urlimage di model produk

--- app/Models/produk.php
+++ app/Models/produk.php
@@ -3,16 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Produk extends Model
 {
     use HasFactory;
+    
     protected $table = 'produk';
     protected $primaryKey = 'id_produk';
     public $incrementing = false;
     protected $keyType = 'string';
+
+    // 1. TAMBAHKAN INI: Agar field 'gambar_url' otomatis muncul di JSON response
+    protected $appends = ['gambar_url'];
 
     protected $fillable = [
         'id_produk',
@@ -28,13 +31,22 @@ class Produk extends Model
         'gambar'
     ];
 
-
+    // url gambar
+    public function getGambarUrlAttribute()
+    {
+        if (!$this->gambar) {
+            return null;
+        }
+        return url('photo/' . $this->gambar); 
+        
+    
+    }
+  
     public function produk() {
         return $this->hasMany(Review::class, 'id_produk');
     }
+    
     public function reviews() {
         return $this->hasMany(Review::class, 'id_review');
     }
-
-    
 }
